ec74c4345e649008c1b9da42e051f6b694beffbc
dann <austindan168@gmail.com>
2025-01-31 13:03:15 +0300

    done css almostt

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import Home from "./Home";
 
 function Layout(){
     return(
@@ -7,6 +8,7 @@ function Layout(){
           <Navbar /> 
           <div>
              <Outlet />
+             < Home />
           </div>      
         </div>
     )
