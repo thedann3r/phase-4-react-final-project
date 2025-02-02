e22032d681206287227283c5ec862de83a4a7ac6
dann <austindan168@gmail.com>
2025-02-02 18:23:15 +0300

    edited

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-import Home from "./Home";
 
 function Layout(){
     return(
@@ -8,7 +7,6 @@ function Layout(){
           <Navbar /> 
           <div>
              <Outlet />
-             < Home />
           </div>      
         </div>
     )
